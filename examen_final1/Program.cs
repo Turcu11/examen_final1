b650c975f8a060a25e083e6a55d6f7158fd795fc
Iosif Turcu <iosif.turcu@emmanuel.ro>
2022-02-07 09:35:52 +0200

    work in progress

--- examen_final1/Program.cs
+++ examen_final1/Program.cs
@@ -6,7 +6,27 @@ namespace examen_final1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Please enter the size of the array: ");
+            int dim = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[dim];
+            for(int i = 0; i < dim; i++)
+            {
+                Console.Write("Fill in the array: ");
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Rec(array);
+        }
+
+        public static int Rec(int[] array)
+        {
+            int sum = 0;
+            if(array.Length <= 0)
+            {
+                return 0;
+            }
+
+            return sum + Rec(array[]);
         }
     }
 }
